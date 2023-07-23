a37b0725ea0706d15e853d1eff3c7ddac7e641fb
hooninpark <hooninpark@gmail.com>
2023-07-24 08:38:37 +0900

    controller

--- _005_Control/_02_switch.c
+++ _005_Control/_02_switch.c
@@ -2,28 +2,29 @@
 
 void main() {
 	char ch;
-	printf("연산자 입력(+, -, *, /, %) >> ");
+
+	printf("scan operator (+, -, *, /, %) >> ");
 	scanf_s("%c", &ch);
 
-	switch (ch) {
+	switch (ch)
+	{
 	case '+':
-		printf("+ 연산자입니다. \n");
+		printf("+ operator\n");
 		break;
 	case '-':
-		printf("- 연산자입니다. \n");
+		printf("- operator\n");
 		break;
 	case '*':
-		printf("- 연산자입니다. \n");
+		printf("* operator\n");
 		break;
 	case '/':
-		printf("- 연산자입니다. \n");
+		printf("/ operator\n");
 		break;
 	case '%':
-		printf("- 연산자입니다. \n");
+		printf("% operator\n");
 		break;
-
 	default:
-		printf("%c >> 이것은 연산자가 아닙니다.", ch);
+		printf("not an operator!\n");
 		break;
 	}
 }
